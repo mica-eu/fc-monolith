a1e38f2ddc4596d5fd70fb688b6ec594e4ce02ee
Micael Lima <5789811+mica-eu@users.noreply.github.com>
2023-08-30 13:06:28 -0300

    feat: add product-adm check stock usecase

--- src/modules/product-adm/facade/product-adm.facade.int.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.int.spec.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 import { ProductModel } from '../repository/product.model';
 import { ProductAdmFacadeInterface } from './product-adm.facade.interface';
-import { ProductAdmFacadeFactory } from '../factory/product-adm-facade.factory';
+import { ProductAdmFacadeFactory } from '../factory/product-adm.facade.factory';
 
 function makeSut(): ProductAdmFacadeInterface {
   return ProductAdmFacadeFactory.create();
@@ -36,5 +36,21 @@ describe('ProductAdmFacade', () => {
     });
     const product = await ProductModel.findOne({ where: { id: '1' } });
     expect(product).not.toBeNull();
+  });
+
+  it('gets the stock of a product', async () => {
+    const sut = makeSut();
+    await sut.addProduct({
+      id: '1',
+      name: 'Product 1',
+      description: 'Product 1 description',
+      purchasePrice: 10,
+      stock: 10,
+    });
+    const stock = await sut.checkStock({ productId: '1' });
+    expect(stock).toEqual({
+      productId: '1',
+      stock: 10,
+    });
   });
 });
---
+++ src/modules/product-adm/usecase/check-stock.usecase.unit.spec.ts
@@ -0,0 +1,45 @@
+import { container } from 'tsyringe';
+import Id from '../../@shared/value-object/id.value-object';
+import Product from '../domain/product.entity';
+import { ProductGateway } from '../gateway/product.gateway';
+import { CheckStockUseCase } from './check-stock.usecase';
+
+class ProductGatewayStub implements ProductGateway {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async add(product: Product): Promise<void> {
+    return Promise.resolve();
+  }
+  find(id: string): Promise<Product> {
+    return Promise.resolve(
+      new Product({
+        id: new Id(id),
+        name: 'any_name',
+        description: 'any_description',
+        purchasePrice: 10,
+        stock: 10,
+      })
+    );
+  }
+}
+
+function makeSut(): CheckStockUseCase {
+  return container.register('ProductGateway', ProductGatewayStub).resolve(CheckStockUseCase);
+}
+
+describe('CheckStockUseCase', () => {
+  it('gets the stock of a product', async () => {
+    const sut = makeSut();
+    const stock = await sut.execute({ productId: '1' });
+    expect(stock).toEqual({
+      productId: '1',
+      stock: 10,
+    });
+  });
+
+  it('calls productGateway with correct params', async () => {
+    const productGatewaySpy = jest.spyOn(ProductGatewayStub.prototype, 'find');
+    const sut = makeSut();
+    await sut.execute({ productId: '1' });
+    expect(productGatewaySpy).toHaveBeenLastCalledWith('1');
+  });
+});
